4e9e2b91c2fa7b5cb324ceee7a7cc6610fd09a3c
Shawn Catanzarite <me@shawncatz.com>
2013-10-11 14:39:58 -0700

    sync version with server

--- lib/mystro/client/version.rb
+++ lib/mystro/client/version.rb
@@ -3,7 +3,7 @@ unless defined?(Mystro::Client::Version)
     module Client
       module Version
         MAJOR  = 0
-        MINOR  = 2
+        MINOR  = 3
         TINY   = 0
         TAG    = nil
         STRING = [MAJOR, MINOR, TINY, TAG].compact.join('.')
